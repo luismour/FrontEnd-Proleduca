309f45728e879ae2c539a60c866c4a2ef23cee8d
Luismour <luismsantos.contato@gmail.com>
2025-06-03 02:01:04 -0300

    vite

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1500, // Ajuste este valor conforme necessário, em KB
+  }})
